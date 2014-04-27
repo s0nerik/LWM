9309634a92760dc36396cb6a4bb2776dd4af7dfa
Alexander Isayenko <sonerik@mail.ua>
2014-04-28 00:40:52 +0300

    Added buttons indication in MainActivity.

--- app/src/main/java/com/lwm/app/ui/activity/MainActivity.java
+++ app/src/main/java/com/lwm/app/ui/activity/MainActivity.java
@@ -7,14 +7,73 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 
+import com.caverock.androidsvg.SVGImageView;
+import com.lwm.app.App;
 import com.lwm.app.R;
 
 public class MainActivity extends ActionBarActivity {
+
+    private SVGImageView localMusicButtonIcon;
+    private SVGImageView remoteMusicButtonIcon;
+
+    private View.OnTouchListener localMusicButtonTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            switch (motionEvent.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    localMusicButtonIcon.setImageAsset("icons/local_music_glow.svg");
+                    Log.d(App.TAG, "MotionEvent.ACTION_DOWN");
+                    break;
+                case MotionEvent.ACTION_UP:
+                    Log.d(App.TAG, "MotionEvent.ACTION_UP");
+                    localMusicButtonIcon.setImageAsset("icons/local_music_.svg");
+                    break;
+            }
+            return false;
+        }
+    };
+
+    private View.OnTouchListener remoteMusicButtonTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            switch (motionEvent.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    remoteMusicButtonIcon.setImageAsset("icons/radio_glow.svg");
+                    Log.d(App.TAG, "MotionEvent.ACTION_DOWN");
+                    break;
+                case MotionEvent.ACTION_UP:
+                    Log.d(App.TAG, "MotionEvent.ACTION_UP");
+                    remoteMusicButtonIcon.setImageAsset("icons/radio_.svg");
+                    break;
+            }
+            return false;
+        }
+    };
+
+    private View.OnClickListener localMusicButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.d(App.TAG, "onClick");
+            localMusicButtonIcon.setImageAsset("icons/local_music_glow.svg");
+            startActivity(new Intent(MainActivity.this, LocalSongChooserActivity.class));
+        }
+    };
+
+    private View.OnClickListener remoteMusicButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.d(App.TAG, "onClick");
+            remoteMusicButtonIcon.setImageAsset("icons/radio_glow.svg");
+            startActivity(new Intent(MainActivity.this, StationChooserActivity.class));
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +85,24 @@ public class MainActivity extends ActionBarActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setCustomView(R.layout.actionbar_main_activity);
         actionBar.setDisplayShowCustomEnabled(true);
+
+        localMusicButtonIcon = (SVGImageView) findViewById(R.id.start_screen_local_music_button_icon);
+        remoteMusicButtonIcon = (SVGImageView) findViewById(R.id.start_screen_remote_music_button_icon);
+
+        View localMusicButton = findViewById(R.id.start_screen_local_music_button);
+        localMusicButton.setOnTouchListener(localMusicButtonTouchListener);
+        localMusicButton.setOnClickListener(localMusicButtonClickListener);
+
+        View remoteMusicButton = findViewById(R.id.start_screen_remote_music_button);
+        remoteMusicButton.setOnTouchListener(remoteMusicButtonTouchListener);
+        remoteMusicButton.setOnClickListener(remoteMusicButtonClickListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        localMusicButtonIcon.setImageAsset("icons/local_music_.svg");
+        remoteMusicButtonIcon.setImageAsset("icons/radio_.svg");
     }
 
     @Override
@@ -50,20 +127,21 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onPlayerTypeChosen(View v){
-        Intent intent;
-        switch(v.getId()) {
-            case R.id.start_screen_local_music_button:
-                intent = new Intent(this, LocalSongChooserActivity.class);
-                break;
-            case R.id.start_screen_remote_music_button:
-                intent = new Intent(this, StationChooserActivity.class);
-                break;
-            default:
-                return;
-        }
-        startActivity(intent);
-    }
+//    public void onPlayerTypeChosen(View v){
+//        Log.d(App.TAG, "onPlayerTypeChosen");
+//        Intent intent;
+//        switch(v.getId()) {
+//            case R.id.start_screen_local_music_button:
+//                intent = new Intent(this, LocalSongChooserActivity.class);
+//                break;
+//            case R.id.start_screen_remote_music_button:
+//                intent = new Intent(this, StationChooserActivity.class);
+//                break;
+//            default:
+//                return;
+//        }
+//        startActivity(intent);
+//    }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
