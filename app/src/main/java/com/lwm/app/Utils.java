6911eb23ed2b72286b96298e361c041b0ec33604
sonerik <sonerik@mail.ua>
2014-08-11 01:12:59 +0300

    Fixed issue with empty albumart.

--- app/src/main/java/com/lwm/app/Utils.java
+++ app/src/main/java/com/lwm/app/Utils.java
@@ -2,14 +2,13 @@ package com.lwm.app;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.ImageView;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class Utils {
 
@@ -30,21 +29,19 @@ public class Utils {
     }
 
     public static void setAlbumArtFromUri(Context context, ImageView view, Uri uri) {
-        InputStream is = null;
         try {
             if (uri != null) {
-                is = context.getContentResolver().openInputStream(uri);
-                Drawable d = Drawable.createFromStream(is, null);
-                view.setImageDrawable(d);
+                Bitmap bmp = MediaStore.Images.Media.getBitmap(context.getContentResolver(), uri);
+                if (bmp != null)
+                    view.setImageBitmap(bmp);
+                else
+                    view.setImageResource(R.drawable.no_cover);
+            } else {
+                view.setImageResource(R.drawable.no_cover);
             }
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+            Log.d(App.TAG, "Unable to set ImageView from URI: " + e.toString());
             view.setImageResource(R.drawable.no_cover);
-        } catch(Exception e) {
-            Log.e(App.TAG, "Unable to set ImageView from URI: " + e.toString());
-        } finally {
-            if (is != null) try {
-                is.close();
-            } catch (IOException ignored) {}
         }
     }
 
