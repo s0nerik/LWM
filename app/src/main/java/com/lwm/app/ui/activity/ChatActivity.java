9a1d940c4cf1a447afcbfb742856ed1375413622
sonerik <sonerik@mail.ua>
2014-10-19 03:09:05 +0300

    Chat activity Action Bar improved.

--- app/src/main/java/com/lwm/app/ui/activity/ChatActivity.java
+++ app/src/main/java/com/lwm/app/ui/activity/ChatActivity.java
@@ -1,15 +1,41 @@
 package com.lwm.app.ui.activity;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.lwm.app.R;
 
 public class ChatActivity extends ActionBarActivity {
 
+    private ActionBar actionBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        actionBar = getSupportActionBar();
+        actionBar.setTitle(R.string.chat_activity_label);
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.chat, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
