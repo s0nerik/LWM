19753c2d7205aa4eb2d3e529f33b4176774c3316
Alex Isaienko <sonerik@mail.ua>
2014-11-08 20:01:42 +0200

    Now playing fragment with Palette.

--- app/src/main/java/com/lwm/app/ui/fragment/NowPlayingFragment.java
+++ app/src/main/java/com/lwm/app/ui/fragment/NowPlayingFragment.java
@@ -1,7 +1,9 @@
 package com.lwm.app.ui.fragment;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.graphics.Palette;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +11,10 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.danh32.fontify.TextView;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.ImageViewBitmapInfo;
 import com.koushikdutta.ion.Ion;
+import com.koushikdutta.ion.bitmap.BitmapInfo;
 import com.lwm.app.R;
 import com.lwm.app.Utils;
 import com.lwm.app.events.player.playback.PlaybackPausedEvent;
@@ -88,7 +93,9 @@ public class NowPlayingFragment extends DaggerFragment {
                 .smartSize(true)
                 .placeholder(R.drawable.no_cover)
                 .error(R.drawable.no_cover)
-                .load(song.getAlbumArtUri().toString());
+                .load(song.getAlbumArtUri().toString())
+                .withBitmapInfo()
+                .setCallback(new BitmapInfoCallback());
 
         mArtist.setText(utils.getArtistName(song.getArtist()));
         mTitle.setText(song.getTitle());
@@ -96,8 +103,18 @@ public class NowPlayingFragment extends DaggerFragment {
 
     }
 
-    public void setPlayButton(boolean playing) {
+    private void setPlayButton(boolean playing) {
         mBtnPlayPause.setImageResource(playing ? R.drawable.ic_av_pause : R.drawable.ic_av_play_arrow);
+    }
+
+    private void applyDefaultStyle() {
+        applyStyle(getResources().getColor(R.color.now_playing_bg), Color.WHITE, Color.WHITE);
+    }
+
+    private void applyStyle(int bgColor, int titleColor, int subtitleColor) {
+        mGlobalLayout.setBackgroundColor(bgColor);
+        mTitle.setTextColor(titleColor);
+        mArtist.setTextColor(subtitleColor);
     }
 
     @OnClick(R.id.btn_play_pause)
@@ -131,6 +148,32 @@ public class NowPlayingFragment extends DaggerFragment {
     @Subscribe
     public void onPlaying(SongPlayingEvent event) {
         mProgressWheel.setProgress((int) (360 * (event.getProgress() / (float) event.getDuration())));
+    }
+
+    private class BitmapInfoCallback implements FutureCallback<ImageViewBitmapInfo> {
+        @Override
+        public void onCompleted(Exception e, ImageViewBitmapInfo result) {
+            BitmapInfo bitmapInfo = result.getBitmapInfo();
+            if (bitmapInfo.bitmaps != null && bitmapInfo.bitmaps.length > 0) {
+                Palette.generateAsync(bitmapInfo.bitmaps[0], new Palette.PaletteAsyncListener() {
+                    @Override
+                    public void onGenerated(Palette palette) {
+                        Palette.Swatch swatch = palette.getVibrantSwatch();
+                        if (swatch != null) {
+                            applyStyle(
+                                    swatch.getRgb(),
+                                    swatch.getTitleTextColor(),
+                                    swatch.getBodyTextColor()
+                            );
+                        } else {
+                            applyDefaultStyle();
+                        }
+                    }
+                });
+            } else {
+                applyDefaultStyle();
+            }
+        }
     }
 
 }
