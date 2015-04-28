8758565e75a1c2b6b4fc45d8acab7e49948c0e34
sonerik <thesonerik@gmail.com>
2015-04-28 23:54:17 +0300

    groovifying!!

--- app/src/main/groovy/app/events/player/playback/control/ChangeSongEvent.groovy
+++ app/src/main/groovy/app/events/player/playback/control/ChangeSongEvent.groovy
@@ -1,16 +1,16 @@
-package app.events.player.playback.control;
+package app.events.player.playback.control
 
+import groovy.transform.CompileStatic;
+
+@CompileStatic
 public class ChangeSongEvent {
 
     public enum Type { NEXT, PREV, TOGGLE_PAUSE, PAUSE, PLAY }
 
-    private Type type;
+    Type type;
 
     public ChangeSongEvent(Type type) {
         this.type = type;
     }
 
-    public Type getType() {
-        return type;
-    }
 }
