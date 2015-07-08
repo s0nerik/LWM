5db4efdf862941208e5a6a256ee91b4b4e631b2a
sonerik <thesonerik@gmail.com>
2015-07-08 23:36:15 +0300

    Refactoring

--- app/src/main/groovy/app/ui/fragment/StationsAroundFragment.groovy
+++ app/src/main/groovy/app/ui/fragment/StationsAroundFragment.groovy
@@ -1,16 +1,14 @@
 package app.ui.fragment
-
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.support.annotation.Nullable
 import android.support.v4.view.ViewPager
 import android.support.v7.widget.Toolbar
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.LinearLayout
 import app.R
 import app.adapter.PlayersAroundPagerAdapter
@@ -20,7 +18,7 @@ import app.helper.wifi.WifiAP
 import app.service.LocalPlayerService
 import app.ui.base.DaggerFragment
 import com.astuetz.PagerSlidingTabStrip
-import com.github.s0nerik.betterknife.annotations.InjectView
+import com.github.s0nerik.betterknife.annotations.InjectLayout
 import com.github.s0nerik.betterknife.annotations.OnClick
 import com.squareup.otto.Bus
 import com.squareup.otto.Produce
@@ -34,109 +32,92 @@ import javax.inject.Inject
 
 @CompileStatic
 @PackageScope(PackageScopeTarget.FIELDS)
-public class StationsAroundFragment extends DaggerFragment {
+@InjectLayout(value = R.layout.fragment_stations_around, injectAllViews = true)
+class StationsAroundFragment extends DaggerFragment {
 
     @Inject
-    Bus bus;
+    Bus bus
 
     @Inject
-    WifiManager wifiManager;
+    WifiManager wifiManager
 
     @Inject
-    WifiAP wifiAP;
+    WifiAP wifiAP
 
-    @InjectView(R.id.toolbar)
-    Toolbar mToolbar;
-    @InjectView(R.id.tabs)
-    PagerSlidingTabStrip mTabs;
-    @InjectView(R.id.pager)
-    ViewPager mPager;
-    @InjectView(R.id.no_wifi_frame)
-    LinearLayout mNoWifiFrame;
+    Toolbar toolbar
+    PagerSlidingTabStrip tabs
+    ViewPager pager
+    LinearLayout noWifiFrame
 
-    private BroadcastReceiver onBroadcast = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent i) {
-            switch (i.getAction()) {
-                case WifiManager.SCAN_RESULTS_AVAILABLE_ACTION:
-                    Debug.d("SCAN_RESULTS_AVAILABLE_ACTION");
-                    bus.post(new WifiScanResultsAvailableEvent(wifiManager.getScanResults()));
-                    break;
-                case WifiManager.WIFI_STATE_CHANGED_ACTION:
-                    Debug.d("WIFI_STATE_CHANGED_ACTION");
-                    bus.post(new WifiStateChangedEvent(wifiManager));
-                    break;
-            }
+    private BroadcastReceiver onBroadcast = { Context context, Intent i ->
+        switch (i.action) {
+            case WifiManager.SCAN_RESULTS_AVAILABLE_ACTION:
+                Debug.d "SCAN_RESULTS_AVAILABLE_ACTION"
+                bus.post new WifiScanResultsAvailableEvent(wifiManager.scanResults)
+                break
+            case WifiManager.WIFI_STATE_CHANGED_ACTION:
+                Debug.d "WIFI_STATE_CHANGED_ACTION"
+                bus.post new WifiStateChangedEvent(wifiManager)
+                break
         }
-    };
+    } as BroadcastReceiver
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getActivity().stopService(new Intent(getActivity(), LocalPlayerService.class));
+    void onCreate(Bundle savedInstanceState) {
+        super.onCreate savedInstanceState
+        activity.stopService new Intent(activity, LocalPlayerService)
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_stations_around, container, false);
-
-        mToolbar.setTitle(getString(R.string.stations_around));
-
-        mPager.setAdapter(new PlayersAroundPagerAdapter(getChildFragmentManager()));
-
-        mTabs.setViewPager(mPager);
-
-        return v;
+    void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated view, savedInstanceState
+        toolbar.title = R.string.stations_around
+        pager.adapter = new PlayersAroundPagerAdapter(childFragmentManager)
+        tabs.viewPager = pager
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-
+    void onResume() {
+        super.onResume()
+        activity.registerReceiver onBroadcast, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+        activity.registerReceiver onBroadcast, new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION)
+        bus.register this
+        toggleNoWifiFrame()
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        getActivity().registerReceiver(onBroadcast, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-        getActivity().registerReceiver(onBroadcast, new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION));
-        toggleNoWifiFrame();
-        bus.register(this);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        getActivity().unregisterReceiver(onBroadcast);
-        bus.unregister(this);
+    void onPause() {
+        super.onPause()
+        activity.unregisterReceiver onBroadcast
+        bus.unregister this
     }
 
     private void toggleNoWifiFrame() {
-        if (!wifiManager.isWifiEnabled()) {
-            mPager.setVisibility(View.GONE);
-            mNoWifiFrame.setVisibility(View.VISIBLE);
+        if (!wifiManager.wifiEnabled) {
+            pager.visibility = View.GONE
+            noWifiFrame.visibility = View.VISIBLE
         } else {
-            mPager.setVisibility(View.VISIBLE);
-            mNoWifiFrame.setVisibility(View.GONE);
+            pager.visibility = View.VISIBLE
+            noWifiFrame.visibility = View.GONE
         }
     }
 
     @Subscribe
-    public void onWifiStateChanged(WifiStateChangedEvent event) {
-        toggleNoWifiFrame();
+    void onWifiStateChanged(WifiStateChangedEvent event) {
+        toggleNoWifiFrame()
     }
 
     @Produce
-    public Toolbar produceToolbar() {
-        return mToolbar;
+    Toolbar produceToolbar() {
+        return toolbar
     }
 
     @OnClick(R.id.no_wifi_frame)
-    public void onNoWifiClicked() {
-        if (wifiAP.isEnabled()) {
-            wifiAP.toggleWiFiAP();
+    void onNoWifiClicked() {
+        if (wifiAP.enabled) {
+            wifiAP.toggleWiFiAP()
         } else {
-            wifiManager.setWifiEnabled(true);
+            wifiManager.wifiEnabled = true
         }
     }
 
