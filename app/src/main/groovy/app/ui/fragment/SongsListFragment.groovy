8a23c986e7b3995554a1ae579b87e449bf50feae
sonerik <thesonerik@gmail.com>
2015-10-18 23:24:13 +0300

    Loading songs in bg

--- app/src/main/groovy/app/ui/fragment/SongsListFragment.groovy
+++ app/src/main/groovy/app/ui/fragment/SongsListFragment.groovy
@@ -1,24 +1,23 @@
 package app.ui.fragment
-
 import android.widget.ProgressBar
 import app.R
 import app.data_managers.SongsManager
 import app.model.Song
 import com.github.s0nerik.betterknife.annotations.InjectLayout
 import com.github.s0nerik.betterknife.annotations.OnClick
-import com.github.s0nerik.betterknife.annotations.Profile
-
 import com.melnykov.fab.FloatingActionButton
 import com.squareup.otto.Bus
 import groovy.transform.CompileStatic
 import groovy.transform.PackageScope
 import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
 import javax.inject.Inject
 
 @CompileStatic
 @InjectLayout(value = R.layout.fragment_list_songs, injectAllViews = true)
-public final class SongsListFragment extends BaseSongsListFragment {
+final class SongsListFragment extends BaseSongsListFragment {
 
     FloatingActionButton fab
     ProgressBar progress
@@ -32,13 +31,16 @@ public final class SongsListFragment extends BaseSongsListFragment {
     Bus bus
 
     @OnClick(R.id.fab)
-    public void onFabClicked() {
+    void onFabClicked() {
         shuffleAll()
     }
 
     @Override
     protected Observable<List<Song>> loadSongs() {
-        songsManager.loadAllSongs().toList()
+        songsManager.loadAllSongs()
+                .toList()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
     }
 
 }
